02cfab6e6f07f6404630f38f1ae3762dc1bb0489
Gabriel de Jesus <gabrieeldejesus@hotmail.com>
2020-07-07 18:36:34 -0300

    slide buttons

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,4 @@
+/*============================================================ MENU ============================================================*/
 const menu = document.getElementById("menu");
 
 function menuToggle() {
@@ -9,4 +10,15 @@ function menuToggle() {
 }
 function closeMenu() {
     menu.style.display = 'none';
+}
+
+/*============================================================ SLIDES ============================================================*/
+const c = (elemento)=>document.getElementById(elemento); // Retornar o item
+
+function arrowRight() {
+    c('slides-area').style.marginLeft = '-10vw';
+}
+
+function arrowLeft() {
+    c('slides-area').style.marginLeft = '0vw';
 }
